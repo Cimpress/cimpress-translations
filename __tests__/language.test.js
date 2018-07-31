57e7dada48d9c606ff086456de775b93db734cfd
Andrej Kincel <andrej.kincel@gmail.com>
2018-07-31 14:10:50 +0200

    Add putLanguageBlob method #6

--- __tests__/language.test.js
+++ __tests__/language.test.js
@@ -51,4 +51,93 @@ describe("for language utility module", () => {
       });
     });
   });
+
+  describe("for findLanguage()", () => {
+    it("returns language object of given ISO 639-2 code", () => {
+      let testLanguages = {
+        "eng": {
+          "639-1": "en",
+          "639-2": "eng",
+          "de": [
+            "Englisch"
+          ],
+          "en": [
+            "English"
+          ],
+          "fr": [
+            "anglais"
+          ],
+          "wikiUrl": "https://en.wikipedia.org/wiki/English_language"
+        },
+        "deu": {
+          "639-1": "de",
+          "639-2": "deu",
+          "639-2/B": "ger",
+          "de": [
+            "Deutsch"
+          ],
+          "en": [
+            "German"
+          ],
+          "fr": [
+            "allemand"
+          ],
+          "wikiUrl": "https://en.wikipedia.org/wiki/German_language"
+        }
+      };
+
+      Object.entries(testLanguages).map(l => {
+        assert.deepEqual(language.findLanguage(l[0]), l[1]);
+      });
+    });
+
+    it("returns language object of given English name of language", () => {
+      let testLanguages = {
+        "English": {
+          "639-1": "en",
+          "639-2": "eng",
+          "de": [
+            "Englisch"
+          ],
+          "en": [
+            "English"
+          ],
+          "fr": [
+            "anglais"
+          ],
+          "wikiUrl": "https://en.wikipedia.org/wiki/English_language"
+        },
+        "German": {
+          "639-1": "de",
+          "639-2": "deu",
+          "639-2/B": "ger",
+          "de": [
+            "Deutsch"
+          ],
+          "en": [
+            "German"
+          ],
+          "fr": [
+            "allemand"
+          ],
+          "wikiUrl": "https://en.wikipedia.org/wiki/German_language"
+        }
+      };
+
+      Object.entries(testLanguages).map(l => {
+        assert.deepEqual(language.findLanguage(l[0]), l[1]);
+      });
+    });
+
+    it("returns null if the language isn't found", () => {
+      let testLanguages = {
+        "Englisch": null,
+        "Cimpress": null
+      };
+
+      Object.entries(testLanguages).map(l => {
+        assert.deepEqual(language.findLanguage(l[0]), l[1]);
+      });
+    });
+  });
 });
